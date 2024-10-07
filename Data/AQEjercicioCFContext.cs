f114c1cccb32a027d7fae0a6c4bfdd376383e1fb
SebasDh56 <amable.quishpe@udla.edu.ec>
2024-10-07 11:24:52 -0500

    3.0

---
+++ Data/AQEjercicioCFContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AQEjercicioCF.Models;
+
+    public class AQEjercicioCFContext : DbContext
+    {
+        public AQEjercicioCFContext (DbContextOptions<AQEjercicioCFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AQEjercicioCF.Models.Burger> Burger { get; set; } = default!;
+    }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AQEjercicioCFContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AQEjercicioCFContext") ?? throw new InvalidOperationException("Connection string 'AQEjercicioCFContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
